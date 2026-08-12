d9889f023aa436637022e61b3bb109b9c71279bb
JeanRodrigues-23 <jeantrabalhodf@gmail.com>
2026-08-11 23:28:32 -0300

    Inserindo funcionalidade de lista vazia

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -7,33 +7,34 @@ function Tasks(props) {
 
   function onSeeDetailsClick(task) {
     const query = new URLSearchParams();
-    query.set("tittle", task.tittle)
-    query.set("description", task.description)
+    query.set("tittle", task.tittle);
+    query.set("description", task.description);
     navigate(`/task?${query.toString()}`);
   }
 
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {props.tasks.length !== 0 ? props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => props.onTaskClick(task.id)}
-            className={`bg-slate-400 p-2 rounded-md text-white w-full ${task.isCompleted && "line-through"}`}
+            className={`bg-slate-400 p-2 rounded-md text-white w-full flex items-center gap-2 ${task.isCompleted && "line-through"}`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.tittle}
           </button>
-          <Button
-            onClick={() => onSeeDetailsClick(task)}
-          >
+          <Button onClick={() => onSeeDetailsClick(task)}>
             <ChevronRightIcon />
           </Button>
-          <Button
-            onClick={() => props.onDeleteTask(task.id)}
-          >
+          <Button onClick={() => props.onDeleteTask(task.id)}>
             <TrashIcon />
           </Button>
         </li>
-      ))}
+      )) : (
+        <div className="flex justify-center">
+          <p className="text-slate-500">Lista vazia</p>
+        </div>
+      )}
     </ul>
   );
 }
